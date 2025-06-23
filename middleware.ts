783748fa62c1a1ae42969f47c17d322567cb396f
Dominik Ferber <dominik.ferber@gmail.com>
2025-06-23 11:42:40 -0400

    use nodejs middleware

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,12 @@
-import { type NextRequest, NextResponse } from 'next/server';
-import { precompute } from 'flags/next';
-import { productFlags } from '@/flags';
-import { getStableId } from './lib/get-stable-id';
-import { getCartId } from './lib/get-cart-id';
+import { type NextRequest, NextResponse } from "next/server";
+import { precompute } from "flags/next";
+import { productFlags } from "@/flags";
+import { getStableId } from "./lib/get-stable-id";
+import { getCartId } from "./lib/get-cart-id";
 
 export const config = {
-  matcher: ['/', '/cart'],
+  matcher: ["/", "/cart"],
+  runtime: "nodejs",
 };
 
 export async function middleware(request: NextRequest) {
@@ -16,22 +17,22 @@ export async function middleware(request: NextRequest) {
   // rewrites the request to the variant for this flag combination
   const nextUrl = new URL(
     `/${code}${request.nextUrl.pathname}${request.nextUrl.search}`,
-    request.url,
+    request.url
   );
 
   // Add a header to the request to indicate that the stable id is generated,
   // as it will not be present on the cookie request header on the first-ever request.
   if (cartId.isFresh) {
-    request.headers.set('x-generated-cart-id', cartId.value);
+    request.headers.set("x-generated-cart-id", cartId.value);
   }
 
   if (stableId.isFresh) {
-    request.headers.set('x-generated-stable-id', stableId.value);
+    request.headers.set("x-generated-stable-id", stableId.value);
   }
 
   // response headers
   const headers = new Headers();
-  headers.append('set-cookie', `stable-id=${stableId.value}`);
-  headers.append('set-cookie', `cart-id=${cartId.value}`);
+  headers.append("set-cookie", `stable-id=${stableId.value}`);
+  headers.append("set-cookie", `cart-id=${cartId.value}`);
   return NextResponse.rewrite(nextUrl, { request, headers });
 }
