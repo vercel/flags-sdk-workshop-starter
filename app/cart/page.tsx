6e8bd41f56f491921ab305a1dc6e98866507923b
Dominik Ferber <dominik.ferber@gmail.com>
2025-06-24 11:19:40 -0400

    make cart sync

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -2,7 +2,7 @@ import { OrderSummary } from "@/app/cart/order-summary";
 import { Main } from "@/components/main";
 import { ShoppingCart } from "@/components/shopping-cart/shopping-cart";
 
-export default async function CartPage() {
+export default function CartPage() {
   return (
     <Main>
       <div className="lg:grid lg:grid-cols-12 lg:items-start lg:gap-x-12 xl:gap-x-16">
